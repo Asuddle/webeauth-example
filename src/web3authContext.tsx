a3bb4dfa32c3f12e51a6111bfc0521190718b594
Ahmad Taimoor Suddle <33666476+Asuddle@users.noreply.github.com>
2025-12-04 07:04:35 +0500

    Initial commit - React Quick Start standalone

---
+++ src/web3authContext.tsx
@@ -0,0 +1,16 @@
+// IMP START - Quick Start
+import { WEB3AUTH_NETWORK } from "@web3auth/modal";
+import { type Web3AuthContextConfig } from "@web3auth/modal/react";
+// IMP END - Quick Start
+
+// IMP START - Dashboard Registration
+const clientId = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
+
+const web3AuthContextConfig: Web3AuthContextConfig = {
+  web3AuthOptions: {
+    clientId,
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+  }
+};
+
+export default web3AuthContextConfig;
