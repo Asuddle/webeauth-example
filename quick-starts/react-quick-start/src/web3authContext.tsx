4092cab651a5fbdd95fe39a728120fb3956e9cb7
Ahmad Taimoor Suddle <33666476+Asuddle@users.noreply.github.com>
2025-12-04 06:46:51 +0500

    fix

--- quick-starts/react-quick-start/src/web3authContext.tsx
+++ quick-starts/react-quick-start/src/web3authContext.tsx
@@ -4,16 +4,13 @@ import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 // IMP END - Quick Start
 
 // IMP START - Dashboard Registration
-const clientId = "BHgArYmWwSeq21czpcarYh0EVq2WWOzflX-NTK-tY1-1pauPzHKRRLgpABkmYiIV_og9jAvoIxQ8L3Smrwe04Lw"; // get from https://dashboard.web3auth.io
-// IMP END - Dashboard Registration
+const clientId = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
 
-// IMP START - Config
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
   }
 };
-// IMP END - Config
 
 export default web3AuthContextConfig;
